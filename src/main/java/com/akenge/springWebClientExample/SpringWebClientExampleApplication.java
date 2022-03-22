a483794ba8cdbf35d8c630ea8007dbb499037485
wilfriedpapt89 <wilfriedpapt@gmail.com>
2022-03-22 19:20:59 +0200

    adding domain class for this example

---
+++ src/main/java/com/akenge/springWebClientExample/SpringWebClientExampleApplication.java
@@ -0,0 +1,13 @@
+package com.akenge.springWebClientExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebClientExampleApplication.class, args);
+	}
+
+}
